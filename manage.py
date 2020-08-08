82bcb15dc37f481a1819e3ff34d7138bec3123b7
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2020-08-08 10:04:42 +0200

    added basic test

---
+++ manage.py
@@ -0,0 +1,18 @@
+from flask.cli import FlaskGroup
+
+from project import app, db
+
+
+cli = FlaskGroup(app)
+
+# This registers a new command, recreate_db, to the CLI 
+# so that we can run it from the command line, 
+# which we'll use shortly to apply the model to the database.
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+if __name__ == '__main__':
+    cli()
---
+++ project/__init__.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, jsonify
+from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+
+# instantiate app
+app = Flask(__name__)
+
+api = Api(app)
+
+# config setting
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+# instantiate db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+
+
+class Ping(Resource):
+
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong'
+        }
+
+
+
+# import sys
+# print(app.config, file=sys.stderr)
+
+api.add_resource(Ping, '/ping')
