0fd54bc76aa8e8874cdd2e46a12f33edbf7369de
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2020-09-04 14:38:59 +0200

    react authentication2

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import About from "./components/About";
 import NavBar from "./components/NavBar";
 import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
+import UserStatus from "./components/UserStatus";
 
 class App extends Component {
   constructor() {
@@ -105,7 +106,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar title={this.state.title} logoutUser={this.logoutUser} />
+        <NavBar
+          title={this.state.title}
+          logoutUser={this.logoutUser}
+          isAuthenticated={this.isAuthenticated} />
         <section className="section">
           <div className="container">
             <div className="columns">
@@ -148,6 +152,11 @@ class App extends Component {
                       />
                     )}
                   />
+                  <Route exact path='/status' render={() => (
+                    <UserStatus
+                      accessToken={this.state.accessToken}
+                      isAuthenticated={this.isAuthenticated} />
+                  )} />
                 </Switch>
               </div>
             </div>
